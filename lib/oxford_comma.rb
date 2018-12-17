f8a2c2ba88d41e5dbd67612788503d8ca5a93891
Ethan Roberts <ejrobe@gmail.com>
2018-12-17 22:59:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    # last = array.last
-    # last_position = array.index(last)
     array[-1] = "and " + array[-1]
     array.join(", ")
   end
