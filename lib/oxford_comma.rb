134b461937d08184c47a7b5799153903cd5cef5d
Ethan Roberts <ejrobe@gmail.com>
2018-12-17 22:59:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array[0]
   elsif array.length == 2
-    array.join("and")
+    array.join(" and ")
   else
     # last = array.last
     # last_position = array.index(last)
